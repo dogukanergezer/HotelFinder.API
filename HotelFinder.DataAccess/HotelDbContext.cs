5ae5d7430d3b392ee82d387ff2476132ac0298c2
dogukanergezer <52700986+dogukanergezer@users.noreply.github.com>
2020-08-23 14:45:09 +0300

    api Commit

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JCOK08U;Database=HotelDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
